f866d5ac32d77319baf59598198c2cee9ee8c405
David Manye <david.manye@gmail.com>
2017-11-28 15:50:30 +0100

    Add info of structure in README.md

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ for this reason, a stub of the `Book Structure Service` is provided, [StubBookSt
 
 This will help you to navigate through the structure of the books without having to think about it's model.
 
+This is the structure created in [StubBookStructureService](src/main/java/com/vicensvives/digital/javahiringtest/services/bookstructure/StubBookStructureService.java):
+
+```
++-- Unit 1 = 9f50d492-03fe-4e42-8717-c3ce87650dd2
+|   +-- Section A = 9e1b818d-7b9d-43ef-b5da-0771de057e81
+|       +-- Activity 1 = 4e405905-91b4-49a9-8fe7-1574cea01580
+|       +-- Activity 2 = 3cafb913-cf92-4aa9-8a83-6be5c3f05c43
+|   +-- Section B = 31011407-974f-4aaf-a153-c00991c2e6a7
+|       +-- Activity 1 = 9a1b3747-af98-481c-ac24-33544200f8b0
+|       +-- Activity 2 = 9838997b-5e07-43e1-9cce-0a75dd27fcd5
+```
+
 
 ## Configuration
 
@@ -18,13 +30,15 @@ and if necessary all extra configurations can be placed there.
 
 
 ## Execute
-The application can be run with just checking out the project.
+The application can be ran with just checking out the project.
 
 ```
 ./gradlew bootRun
 ```
 
-This command will execute the application listening the 8989 port, and can be accessed via [http://localhost:8989/](http://localhost:8989/)
+This command will execute the application listening the 8989 port, and can be accessed via [http://localhost:8989/](http://localhost:8989/).
+
+You can see in logs `c.v.digital.javahiringtest.Application   : Started Application in X seconds` to know when api is available. 
 
 The default port can be configured at the configuration file.
 
--- src/main/java/com/vicensvives/digital/javahiringtest/services/bookstructure/StubBookStructureService.java
+++ src/main/java/com/vicensvives/digital/javahiringtest/services/bookstructure/StubBookStructureService.java
@@ -1,6 +1,5 @@
 package com.vicensvives.digital.javahiringtest.services.bookstructure;
 
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toSet;
 
 import com.google.common.collect.ImmutableMap;
@@ -11,32 +10,35 @@ import java.util.Set;
 
 public class StubBookStructureService implements BookStructureService {
 
-    public static final UnitId UNIT_ID = new UnitId("9f50d492-03fe-4e42-8717-c3ce87650dd2");
+    public static final UnitId UNIT_ID_1 = new UnitId("9f50d492-03fe-4e42-8717-c3ce87650dd2");
     public static final SectionId SECTION_ID_A = new SectionId("9e1b818d-7b9d-43ef-b5da-0771de057e81");
     public static final SectionId SECTION_ID_B = new SectionId("31011407-974f-4aaf-a153-c00991c2e6a7");
     public static final ActivityId ACTIVITY_ID_A_1 = new ActivityId("4e405905-91b4-49a9-8fe7-1574cea01580");
+    public static final ActivityId ACTIVITY_ID_A_2 = new ActivityId("3cafb913-cf92-4aa9-8a83-6be5c3f05c43");
+    public static final ActivityId ACTIVITY_ID_B_1 = new ActivityId("9a1b3747-af98-481c-ac24-33544200f8b0");
+    public static final ActivityId ACTIVITY_ID_B_2 = new ActivityId("9838997b-5e07-43e1-9cce-0a75dd27fcd5");
 
     private final Map<ActivityId, SectionId> activityToSection = ImmutableMap.<ActivityId, SectionId>builder()
         .put(ACTIVITY_ID_A_1, SECTION_ID_A)
-        .put(new ActivityId("3cafb913-cf92-4aa9-8a83-6be5c3f05c43"), SECTION_ID_A)
-        .put(new ActivityId("9a1b3747-af98-481c-ac24-33544200f8b0"), SECTION_ID_B)
-        .put(new ActivityId("9838997b-5e07-43e1-9cce-0a75dd27fcd5"), SECTION_ID_B)
+        .put(ACTIVITY_ID_A_2, SECTION_ID_A)
+        .put(ACTIVITY_ID_B_1, SECTION_ID_B)
+        .put(ACTIVITY_ID_B_2, SECTION_ID_B)
         .build();
 
     private final Map<SectionId, UnitId> sectionToUnit = ImmutableMap.<SectionId, UnitId>builder()
-        .put(SECTION_ID_A, UNIT_ID)
-        .put(SECTION_ID_B, UNIT_ID)
+        .put(SECTION_ID_A, UNIT_ID_1)
+        .put(SECTION_ID_B, UNIT_ID_1)
         .build();
 
 
     @Override
     public Optional<SectionId> getSectionOfActivity(ActivityId activityId) {
-        return ofNullable(activityToSection.get(activityId));
+        return Optional.ofNullable(activityToSection.get(activityId));
     }
 
     @Override
     public Optional<UnitId> getUnitOfSection(SectionId sectionId) {
-        return ofNullable(sectionToUnit.get(sectionId));
+        return Optional.ofNullable(sectionToUnit.get(sectionId));
     }
 
     @Override
